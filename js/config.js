511b4e96f5eb654791bc8563496af8e7b87d63b8
Adam Hudson <adamnubbz@gmail.com>
2023-04-24 15:34:57 -0500

    migrating from AWS CodeCommit to Github

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-2_lMjxbHAg4", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7sl48m03dpf35jhnhi42r1g0c2", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://2rg9c9s1uk.execute-api.us-east-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
